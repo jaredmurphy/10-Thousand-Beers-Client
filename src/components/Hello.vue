6dfd9a6c58024ecb7999f3c91d3d17666c6a5c85
Jared Murphy <jaredcmurphy@gmail.com>
2017-03-24 22:07:04 -0400

    first commit - internal updates work

---
+++ src/components/Hello.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="beers">
+      <h1>{{ beersCount }}</h1>
+      <h2>Beers Left</h2>
+    </div>
+    <div class="button">
+      <button v-on:click="beerPurchased()">Beer Purchased</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default {
+  name: 'hello',
+  data () {
+    return {
+      beersCount: 10000
+    }
+  },
+  methods: {
+    beerPurchased: function() {
+      this.beersCount = this.beersCount - 1;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
